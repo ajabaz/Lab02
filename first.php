b8567b73f40bfb5e3f9e8eadd99e723bad7998fd
asfand <ajabaz@uclan.ac.uk>
2021-04-15 15:34:56 +0100

    first commit

---
+++ first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<HEAd>
+<TITlE>MY FiRST pAGE</TITlE>
+</HEAd>
+<Body>
+<?Php
+	ECHO "<H1>HERe iS SOME HTML</H1>";
+?>
+</Body>
+</html>
+
+
+
---
+++ hello.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>my first page</title>
+</head>
+<body>
+<?php
+	echo "<h1>here is some html</h1>";
+	echo "<p>hello".$_GET['name']. " ,PLEASED TO MEET YOU!</p>";
+?>
+</body>
+</html>
